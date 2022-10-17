a2f5c0ab2022aa8e7d3defbf4604c3fa9e2cac8e
krisna <krishna.k@shellkode.com>
2022-10-17 07:21:09 +0000

    new

--- changeset.sql
+++ changeset.sql
@@ -1,9 +1,9 @@
---liquibase formatted sql
+-- liquibase formatted sql changeLogId:2662b1a0-34bb-498d-a91f-293950a0e74c
 
---changeset skemp1:001
-create table skemp1 (
+--changeset skemp2:002
+create table skemp2 (
 id int primary key,
 name varchar(255)
 );
 
---rollback drop table skemp1
+--rollback drop table skemp2

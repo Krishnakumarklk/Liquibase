82d0f4894fa2677bd9fa6811543476ab7aa94572
krisna <krishna.k@shellkode.com>
2022-10-20 07:00:12 +0000

    new

--- changeset.sql
+++ changeset.sql
@@ -1,9 +1,9 @@
 -- liquibase formatted sql 
 
---changeset mysql05:005
-create table mysql05 (
-id int,
-name varchar(255)
-);
+--changeset mysql05sp:005
+CREATE PROCEDURE mysql05insert(IN eid int,IN ename varchar(50))
+begin
+insert into mysql05 (id,name) values(@eid,@ename);
+end
 
 --rollback drop table mysql05

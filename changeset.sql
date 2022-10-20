e85ca0182f148386f6c2002c2e9574abc4494955
krisna <krishna.k@shellkode.com>
2022-10-20 06:37:30 +0000

    new

--- changeset.sql
+++ changeset.sql
@@ -1,5 +1,9 @@
 -- liquibase formatted sql 
 
 --changeset mysql04spdrop:004
-drop procedure mysql04insert;
+CREATE PROCEDURE mysql04insert(IN eid int,IN ename varchar(50))
+begin
+insert into mysql04 (id,name) values(@eid,@ename);
+end
+
 --rollback drop table mysql04

ec0770e73a067cf354a6808fb17013c1a2c34221
krisna <krishna.k@shellkode.com>
2022-10-20 06:24:06 +0000

    new

--- changeset.sql
+++ changeset.sql
@@ -1,6 +1,6 @@
 -- liquibase formatted sql 
 
---changeset mysql04:004
+--changeset mysql04sp:004
 create procedure mysql04insert(eid int,ename varchar(50))
 begin 
 insert into mysql04 values(@eid,@ename);

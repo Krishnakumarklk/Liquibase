bfb3b4f91cf4c44dc1d9754490d3f16b5886cb72
krisna <krishna.k@shellkode.com>
2022-10-20 07:31:24 +0000

    new

--- changeset.sql
+++ changeset.sql
@@ -1,13 +1,5 @@
 -- liquibase formatted sql 
 
---changeset psqltestsp:006
-Create OR REPLACE PROCEDURE psqltestinsert (eid INOUT INT, name varchar(100))
-
-LANGUAGE plpgsql AS
-
-$$ BEGIN
-
-INSERT INTO test (id,name) Values (eid, name) RETURNING  ID INTO eid;
-
-END $$;
+--changeset psqltestspx:006
+CALL psqltestinsert (102,'Postgresql');
 --rollback drop table test
